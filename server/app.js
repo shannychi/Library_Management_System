17d15bb54ea3de8c3e6f8a3d299657b7a1c64c93
Edeh sharon <Pearllizzy196@gmail.com>
2024-06-03 11:00:25 +0100

    updated cors

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8000
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://library-management-system2.netlify.app/',
     credentials: true
 }))
 app.use(bodyParser.urlencoded({extended: false}))

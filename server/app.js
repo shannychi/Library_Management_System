a602b17db2de853beec4d0e9105515dfffbe8f2a
Edeh sharon <Pearllizzy196@gmail.com>
2024-06-06 13:13:37 +0100

    updated app.js

--- server/app.js
+++ server/app.js
@@ -1,54 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const cookParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const path = require('path')
 
-const  BookRoutes = require('./routes/Book/bookRoute');
-const  userRoute = require('./routes/Book/userRoute');
+const BookRoutes = require('./routes/Book/bookRoute');
+const userRoute = require('./routes/Book/userRoute');
 
 const app = express();
 const port = process.env.PORT || 8000
 
-
+// CORS setup
 var whitelist = ['http://localhost:5173', 'https://library-management-system2.netlify.app'];
 var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // Allow non-browser requests
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-  }
+  },
+  credentials: true // Allow credentials
+};
 
+app.use(cors(corsOptions)); // Use CORS middleware with the options object
 
-app.use(cors({
-    corsOptions,
-    credentials: true 
-  }));
-
-
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cookParser())
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
-//datebase connection
-const mogoDb = process.env.MONGODB_URL
-mongoose.connect(mogoDb).then(console.log("connected to database"));
+// Database connection
+const mongoDb = process.env.MONGODB_URL
+mongoose.connect(mongoDb).then(() => console.log("Connected to database"));
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
-
-
+// Routes
 app.use('/book', BookRoutes);
 app.use('/user', userRoute)
 
-
-app.listen(port,  function(err) {
-    console.log("app listenning on port 8000", err)
-})
+app.listen(port, function (err) {
+  console.log("App listening on port", port, err || '')
+});

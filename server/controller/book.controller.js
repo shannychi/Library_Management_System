2bf66cf57c81a68301b5fa0f613e8f3136321d98
Edeh sharon <Pearllizzy196@gmail.com>
2024-06-12 19:24:41 +0100

    updated code

--- server/controller/book.controller.js
+++ server/controller/book.controller.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '/uploads')); 
+    cb(null, path.join(__dirname, 'uploads')); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); // unique file names
